737d72b62459cf324548b45074545b3933d81318
Yurii Makoveichuk <yuriimakoveichuk@gmail.com>
2024-11-22 17:52:19 +0200

    added img, env,pullrequests-list

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,14 +1,3 @@
-import { Container } from './Container/Container.jsx';
-import { Section } from './Section/Section.jsx';
-
 export const App = () => {
-  return (
-    <Section>
-      <Container>
-        <div>
-          <h1>final-project-MobileTeam</h1>
-        </div>
-      </Container>
-    </Section>
-  );
+  return <></>;
 };
